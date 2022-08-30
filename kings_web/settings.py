dd26bece96912e43d67e30a2915499a32b3b600c
JonasFiechter <jonasrfiechter@gmail.com>
2022-08-30 10:52:00 -0300

    added_landing_page_app

--- kings_web/settings.py
+++ kings_web/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'kings_web.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'kings_web/templates/static'],
+        'DIRS': [BASE_DIR / 'kings_web/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,6 +119,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'kings_web/templates/static',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
