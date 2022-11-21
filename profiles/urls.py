54f5d253bf1fe79bb4b2ad57e4f1436ead8caca2
Jonas Fiechter <jonasrfiechter@gmail.com>
2022-11-20 22:17:08 -0300

    styling, added sign_up html and view

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import profiles_login_view
+from .views import profiles_login_view, profiles_sign_up_view
 
 urlpatterns = [
-    path('login/', profiles_login_view, name='profiles')
+    path('login/', profiles_login_view, name='login'),
+    path('sign_up/', profiles_sign_up_view, name='sign_up'),
 ]
--- profiles/views.py
+++ profiles/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 
 def profiles_login_view(request):
     return render(request, 'profiles/login.html')
+
+def profiles_sign_up_view(request):
+    return render(request, 'profiles/sign_up.html')
