e9c0ce1497d0fa856bb2323316a4256bb8e6ca26
Jonas Fiechter <jonasrfiechter@gmail.com>
2022-11-20 23:06:53 -0300

    login view working

--- profiles/views.py
+++ profiles/views.py
@@ -1,9 +1,21 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login
 
 # Create your views here.
 
 def profiles_login_view(request):
-    return render(request, 'profiles/login.html')
+    username = request.POST.get('email')
+    password = request.POST.get('password')
+    user = authenticate(request, username=username, password=password)
+    message = ''
+
+    if user is not None:
+        login(request, user)
+        #redirect
+    if request.method == 'POST' and not user:
+        message = 'Usuário ou senha inválidos'
+
+    return render(request, 'profiles/login.html', {'message': message})
 
 def profiles_sign_up_view(request):
     return render(request, 'profiles/sign_up.html')
